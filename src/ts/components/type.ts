0ef8ff88aaa0122901ccd6264f975c4f7324e50d
satyam-seth <satyam1998.1998@gmail.com>
2023-08-16 00:41:18 +0530

    add space in DialpadButtonConfig for better readability

--- src/ts/components/type.ts
+++ src/ts/components/type.ts
@@ -6,12 +6,16 @@
 export default interface DialpadButtonConfig {
   // unique string for namepacing
   namespace: string;
+
   // aria label e.g. One, Two, Three etc
   ariaLabel: string;
+
   // title e.g. 1,2,4,* etc
   title: string;
+
   // subtite e.g. ABC, CDE, GHI etc
   subtitle?: string;
-  // on button click handler
+
+  // button click handler
   onClick: () => void;
 }

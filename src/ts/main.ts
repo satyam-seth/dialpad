aa4ff1fc0d9e1d9c31de2bae7fc2f742ae9b7c5c
satyam-seth <satyam1998.1998@gmail.com>
2023-08-15 19:53:12 +0530

    add dialpad button skeleton

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,4 +1,21 @@
-import hello from './demo';
+import DialpadButton from './layout/buttons';
 
-console.log(hello);
-console.log('chalo');
+window.onload = () => {
+  // eslint-disable-next-line no-console
+  console.log('dialpad');
+
+  // create button instance
+  const btn = new DialpadButton({
+    namespace: 'one',
+    ariaLabel: 'One',
+    title: '1',
+    subtitle: 'abc',
+    onClick: () => {
+      // eslint-disable-next-line no-console
+      console.log('clicked on 1');
+    },
+  });
+
+  // build button
+  btn.build(document.body);
+};

c473acfcf5787bf358de653890f4f47d144c2008
satyam-seth <satyam1998.1998@gmail.com>
2023-12-17 21:00:04 +0530

    add a example e2e test

---
+++ e2e/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
